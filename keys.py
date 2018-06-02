f05705008109e86b4b827504876beb029d860e1d
Jonathan Mercado <jonathanmercado.dev@gmail.com>
2018-06-01 23:40:36 -0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,7 +1,8 @@
 from twython import Twython
+
 #api keys
-API_KEY= ''
-API_SECRET=  ''
-API_TOKEN= ''
-API_TOKEN_SECRET= ''
+API_KEY= 'your keys go between these two apostrophes'
+API_SECRET= 'your keys go between these two apostrophes'
+API_TOKEN= 'your keys go between these two apostrophes'
+API_TOKEN_SECRET= 'your keys go between these two apostrophes'
 api = Twython(API_KEY, API_SECRET, API_TOKEN, API_TOKEN_SECRET)
